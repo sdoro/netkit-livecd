018e51491a1012e7dd8881b75ce9581647e10b94
Sandro Doro <sandro.doro@gmail.com>
2014-10-10 11:54:51 +0200

    Fixed install problem for ca-certificates.

--- tips/files/mk-django.sh
+++ tips/files/mk-django.sh
@@ -2,6 +2,7 @@
 wget --no-check-certificate https://github.com/sdoro/netkit-livecd/raw/master/tips/files/devDjango.deb.tgz
 # Install all downloaded packages
 tar zxf devDjango.deb.tgz
+dpkg -i ca-c*.deb
 dpkg -i *.deb
 # cleanup (before next reboot)
 rm -f *.deb
